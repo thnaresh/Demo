31f6c9cbee6262efe2749a25fa08e6f126ac8e97
naresh <naresh.tellapuri33@gmail.com>
2023-02-02 11:20:32 +0530

    general demo project

---
+++ src/com/demo/ClassA.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class ClassA {
+	int a = 10;
+	static int b = 40;
+	
+	public void meth1() {
+		int c = a+b;
+		System.out.println("the output is "+c);
+		
+	}
+
+	public static void main(String[] args) {
+      ClassA n = new ClassA();
+      
+		n.meth1();
+	}
+
+}
